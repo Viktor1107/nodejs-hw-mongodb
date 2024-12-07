c33fa3c62c36eb23e4cc23e53956809e69980fb7
Viktor1107 <162905444+Viktor1107@users.noreply.github.com>
2024-12-07 22:41:48 +0100

    add:post,puth,delete

--- src/server.js
+++ src/server.js
@@ -3,7 +3,9 @@ import pino from 'pino-http';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import contactsRouter from './routers/contacts.js'
+import {notFoundHandler} from './middlewares/notFoundHandler.js'
+import { errorHandler } from './middlewares/errorHandler.js';
 
 
 
@@ -22,72 +24,11 @@ app.use(
 	}),
   );
 
-  app.get('/contacts', async (req, res) => {
-      const contacts = await getAllContacts();
+app.use(contactsRouter);
 
-	  res.status(200).json({
-		status: 200,
-		message: "Successfully found contacts!",
-		data: contacts
-	  })
-  });
+app.use('*', notFoundHandler);
 
-
-  
-  app.get('/contacts/:contactId', async (req, res, next) => {
-	const{contactId} = req.params;
-	
-
-	// if (!mongoose.Types.ObjectId.isValid(contactId)) {
-	// 	return res.status(400).json({
-	// 	  message: 'Invalid contact ID format',
-	// 	});
-	//   }
-	  try {
-		const contact = await getContactById(contactId)
-
-	if(!contact){
-		res.status(404).json({
-			message: 'Contacts not found'	
-		});
-		return;
-	}
-	res.status(200).json({
-		status: 200,
-		message: "Successfully found contact with id {contactId}!",
-		data: contact
-	})
- } catch (err){
-		next(err)
-	}
-
-  });
-
-
-app.use((req, res, next)=>{
-	console.log(`Time: ${new Date().toLocaleString()}`);
-  next();
-}
-);
-
-app.get('/',(req, res)=>{
-	res.json({
-		message: 'Hello world!'
-	})
-});
-
-app.use('*', (req, res, next) => {
-	res.status(404).json({
-	  message: 'not found',
-	});
-  })
-
-app.use((err, req, res, next) => {
-	console.error('Unhandled error:', err);
-	res.status(500).json({
-	  message: 'Something went wrong'|| 'Internal Server Error',
-	});
-  });
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
