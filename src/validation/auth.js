5772579a44829e8afd58c8d992c45007d026e0ff
Viktor1107 <162905444+Viktor1107@users.noreply.github.com>
2024-12-29 17:03:10 +0100

    one

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -1,13 +1,12 @@
-import Joi from "joi";
+import Joi from 'joi';
 
-export const registerUserSchema = Joi.object({ 	name: Joi.string().min(3).max(20).required(),
-	email: Joi.string().email().required(),
-	password: Joi.string().required(),
-	}
-);
+export const registerUserSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
 
 export const loginUserSchema = Joi.object({
-	email: Joi.string().email().required(),
-	password: Joi.string().required(),
-  }
-);
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
